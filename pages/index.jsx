ef088aa0492b7ad158030e19973932abc425af26
Rogerio Battistoni Filho <rogeriobf.dev@gmail.com>
2021-12-03 15:18:06 -0300

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import CardWeather from './cardWeather/cardWeather'
 
 export default function Home() {
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-tl from-purple-100 to-yellow-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
       <Head>
         <title>Weather time - Rogerbatt</title>
         <link rel="icon" href="/favicon.ico" />
