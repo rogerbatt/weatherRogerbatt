e61623531073116cc659b51892f67e109ba4afd3
Rogerio Battistoni Filho <rogeriobf.dev@gmail.com>
2021-12-03 15:17:39 -0300

    Add files via upload

--- pages/cardWeather/cardWeather.jsx
+++ pages/cardWeather/cardWeather.jsx
@@ -6,29 +6,25 @@ import {useGeolocation} from 'react-use';
 export default function CardWeather() {
   const location = useGeolocation();
   const [user, setUser] = useState();
-  
+  const icon = user?.weather[0].icon;
 
   useEffect(() => {
-    setTimeout(() => {
-      api
-      .get(`/weather?lat=${location.latitude}&lon=${location.longitude}&lang=pt_br&units=metric&appid=5c0be100150d01b142fa961f22fa1b37`)
-      .then((response) => setUser(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-    }, 1000);
+    api
+    .get(`/weather?lat=${location.latitude}&lon=${location.longitude}&lang=pt_br&units=metric&appid=5c0be100150d01b142fa961f22fa1b37`)
+    .then((res) => setUser(res.data))
+    .catch((err) => {
+      console.error("Problem - The API is requesting out of time" + err);
+    })
   }, [location.longitude, location.latitude]);
-
 
   return (
     <div className="flex flex-col items-center justify-center">
         <div className="grid overflow-hidden grid-cols-2 grid-rows-3 gap-5">
-            <div className="flex flex-col items-center justify-center rounded-lg m-2 box row-span-3 col-start-1 hover:ring-4 hover:ring-yellow-100 bg-gradient-to-b from-yellow-200 to-yellow-400 p-10">
+            <div className="flex flex-col items-center justify-center rounded-lg m-2 box row-span-3 col-start-1 hover:ring-4 hover:ring-blue-300 bg-gradient-to-b from-blue-500 to-blue-600 text-blue-100">
                 <img 
                 className="w-32"
-                src={"https://openweathermap.org/img/wn/" + user?.weather[0].icon + "@2x.png"}
+                src={"https://openweathermap.org/img/wn/" + icon + "@4x.png"}
                 />
-                
                 <div className="text-5xl mb-10 text-center">
                   <h1>{user?.weather[0].main}</h1>
                   <h1>{user?.main.temp}°</h1>
@@ -46,7 +42,7 @@ export default function CardWeather() {
             <div className="box row-start-1 col-start-2 p-5 m-2 rounded-lg hover:ring-4 hover:ring-blue-300 bg-gradient-to-b from-blue-500 to-blue-600 text-blue-100">
                 <div className="flex">
                 <LightningBoltIcon className="w-5"/>
-                    Tempo                    
+                    Weather                    
                 </div>
                 <ul className="mt-2">
                   <li className="text-red-300">Temp.Max - {user?.main.temp_max}°</li>
@@ -59,7 +55,7 @@ export default function CardWeather() {
             <div className="box row-start-2 col-start-2 p-5 m-2 rounded-lg hover:ring-4 hover:ring-blue-300 bg-gradient-to-b from-blue-500 to-blue-600 text-blue-100">
                 <div className="flex">
                 <CloudIcon className="w-5"/>
-                    Ventos                    
+                    Wind       
                 </div>
                 <ul className="mt-3">
                   <li>Ventos: {user?.wind.speed} KM/h</li>
@@ -73,7 +69,7 @@ export default function CardWeather() {
             <div className="box row-start-3 col-start-2 p-5 m-2 rounded-lg hover:ring-4 hover:ring-blue-300 bg-gradient-to-b from-blue-500 to-blue-600 text-blue-100">
                 <div className="flex">
                 <TrendingUpIcon className="w-5"/>
-                    Status                    
+                    Extra                    
                 </div>
                 <ul className="mt-3">
                   <li>Nascer do sol: {user?.sys.sunrise}</li>
